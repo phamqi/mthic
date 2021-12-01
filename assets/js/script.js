e09ad16bc09f1c347befccfa1692c43447d1e7bf
phamqi <phamquy2425@gmail.com>
2021-12-01 18:25:19 +0700

    mthic first commit

---
+++ assets/js/script.js
@@ -0,0 +1,94 @@
+
+const nav = () => {
+    const  toggler = document.querySelector('.header__toggle-berger');
+    const  nav = document.querySelector('.header__nav');
+    const navItem = document.querySelectorAll('.header__nav-list li');
+    const overlay = document.getElementById('header-overlay');
+    const search_modal = document.getElementById('search-modal');
+    const btn_cs = document.getElementById('btn-cs');
+
+    toggler.addEventListener('click', ()=>{
+        nav.classList.toggle('header__nav--show');
+        overlay.classList.toggle('header__overlay--show');
+        toggler.classList.toggle('trans');
+
+        navItem.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation ='';
+            } else {
+                link.style.animation = `nav-itemFade 0.3s ease forwards ${index / 5 + 0.2}s`;
+            }
+        });
+      });
+      overlay.addEventListener('click', ()=>{
+      nav.classList.remove('header__nav--show');
+      overlay.classList.remove('header__overlay--show');
+      toggler.classList.remove('trans');
+      navItem.forEach((link, index) => {
+        link.style.animation = '';
+      });
+      search_modal.style.transform = ''
+    });
+    
+    document.querySelectorAll('.btn-search').forEach(item => {
+      item.addEventListener('click', () => {
+        search_modal.style.transform = 'translateY(0%)'
+        overlay.classList.toggle('header__overlay--show');
+      });
+    });
+    btn_cs.addEventListener('click', ()=>{
+      search_modal.style.transform = ''
+      overlay.classList.remove('header__overlay--show');
+  })
+}
+nav();
+
+// var slideIndex = 1;
+// showSlides(slideIndex);
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("main__carousel-item");
+//   var dots = document.getElementsByClassName("main__carousel-dot");
+//   if (n > slides.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";  
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" dot--active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " dot--active";
+// }
+ 
+// document.getElementById("carousel-prev").addEventListener('click', ()=>{
+//     plusSlides(-1);
+//   });
+// document.getElementById("carousel-next").addEventListener('click', ()=>{
+//     plusSlides(1);
+//   });
+// var slideIndex = 0;
+// showSlidespers();
+// function showSlidespers() {
+//   var i;
+//   const slides = document.getElementsByClassName("main__carousel-item");
+//   var dots = document.getElementsByClassName("main__carousel-dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//     dots[i].classList.remove("dot--active");
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].classList.add("dot--active")
+//   setTimeout(showSlidespers, 5000); 
+// }
+
+
---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Walletstore Theme Customizer
+ *
+ * @package Walletstore
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function walletstore_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'walletstore_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'walletstore_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'walletstore_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function walletstore_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function walletstore_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function walletstore_customize_preview_js() {
+	wp_enqueue_script( 'walletstore-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'walletstore_customize_preview_js' );
